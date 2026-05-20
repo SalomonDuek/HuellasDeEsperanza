c246a5e4bf890efbc73b21660358484a470f4119
SalomonDuek <46875033@comunidadort.edu.ar>
2026-05-20 19:30:03 -0300

    "Estructura inicial MVC Huellas de Esperanza"

---
+++ HuellasDeEsperanza/HuellasDeEsperanza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HuellasDeEsperanza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
